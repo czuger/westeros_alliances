7aa2e6b6d7ac2bd89d04a09036ad6a5ac21cc072
Zuger Cédric <zuger.cedric@gmail.com>
2016-05-17 17:07:54 +0200

    Factory girl as default fixtures.

--- lib/westeros_alliances/engine.rb
+++ lib/westeros_alliances/engine.rb
@@ -1,5 +1,10 @@
 module WesterosAlliances
   class Engine < ::Rails::Engine
     isolate_namespace WesterosAlliances
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
+
   end
 end
